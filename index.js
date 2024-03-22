c3e081857faf4e7b13d88af0a3431585de428b72
Abdallah Doleh <94862609+abdullah-doleh@users.noreply.github.com>
2024-03-22 22:35:17 +0000

    add zombie

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 import * as PIXI from "pixi.js";
-//import Victor from "victor";
-//import Matter from "matter-js";
+import Victor from "victor";
+import Player from "./player";
+import zombie from "./zombie.js";
+import Spawner from "./spawner.js";
 
-let canvasSize = 256;
+
+ 
+const canvasSize = 512;
 const canvas = document.getElementById("mycanvas");
 const app = new PIXI.Application({
   view: canvas,
   width: canvasSize,
   height: canvasSize,
   backgroundColor: 0x5c812f,
+  
 });
 
-let squareWidth = 32;
-const square = new PIXI.Sprite(PIXI.Texture.WHITE);
-square.anchor.set(0.5);
-square.position.set(app.screen.width / 2, app.screen.height / 2);
-square.width = square.height = squareWidth;
-square.tint = 0xea985d;
 
-app.stage.addChild(square);
+let player = new Player({app});
+let zSpawner = new Spawner({create:()=> new zombie({app,player})});
+
+
+app.ticker.add((delta)=>{
+  
+   player.update();
+   
+    zSpawner.spawns.forEach((zombie) => zombie.update());
+});
+
+ 
